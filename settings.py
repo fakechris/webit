8af8f937be3f5ae1621ee7bbe788d82b02fb3256
Chris Song <chris__song@hotmail.com>
2008-05-10 11:53:03 -0400

    change path

--- settings.py
+++ settings.py
@@ -35,7 +35,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/var/www/webit/media/'
+MEDIA_ROOT = '/var/www/webit/store/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
