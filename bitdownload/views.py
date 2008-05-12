aea33bd16c9905e2037d29b278ad3ec0f44acd77
Chris Song <fakechris@gmail.com>
2008-05-12 06:16:59 -0400

    add bitdownload and change url

---
+++ bitdownload/views.py
@@ -0,0 +1,17 @@
+# Create your views here.
+
+from django.conf import settings
+from django.http import HttpResponse, Http404
+
+from webit.btl.storage import StorageManage
+
+def get_data(request, torrent, index):
+    # TODO: caching meta data
+    torrent_file = settings.MEDIA_ROOT + "torrent/" + torrent
+    data_path = settings.MEDIA_ROOT + "data/" + torrent + "/"
+    sm = StorageManage(torrent_file, data_path)
+    r = sm.read(index * sm.metainfo.piece_length, sm.metainfo.piece_length)
+    return HttpResponse(r)
+    
+
+
